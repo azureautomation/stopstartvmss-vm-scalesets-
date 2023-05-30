a0ffa0b6faed91363ddb9c20cdad7bc9467a5975
Chetankaur <135039209+Chetankaur@users.noreply.github.com>
2023-05-30 20:24:12 +0530

    Update Stop-Start-VMSS.ps1

--- Stop-Start-VMSS.ps1
+++ Stop-Start-VMSS.ps1
@@ -4,32 +4,48 @@ workflow Stop-Start-VMSS
     (   
         [Parameter(Mandatory=$true)]
         [String]
+        $SubscriptionId,
+        [Parameter(Mandatory=$true)]
+        [String]
         $AzureResourceGroup,
 	[Parameter(Mandatory=$true)][ValidateSet("Start","Stop")]
         [String]
 	$Action
     )
-    
-    $credential = Get-AutomationPSCredential -Name 'AzureCredential'
-    Login-AzureRmAccount -Credential $credential
-    $selectedsubscription = Get-AutomationVariable -Name 'AzureSubscriptionId'
 
-    Select-AzureRmSubscription -SubscriptionId $selectedsubscription
+    # Ensures you do not inherit an AzContext in your runbook
+        Disable-AzContextAutosave -Scope Process
+
+        try
+        {
+            "Logging in to Azure..."           
+
+    # Connect to Azure with system-assigned managed identity. 
+    # Please enable appropriate RBAC permissions to the system identity of this automation account. Otherwise, the runbook may fail...
+            $AzureContext = (Connect-AzAccount -Identity).context
+
+    # set and store context
+            $AzureContext = Set-AzContext -Subscription $SubscriptionId
+        }
+        catch {
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
 
     if($Action -eq "Stop")
     {
 		Write-Output "Stopping VMSS in '$($AzureResourceGroup)' resource group";
-        foreach -parallel ($name in (Get-AzureRmVmss -ResourceGroupName $AzureResourceGroup).Name)
+        foreach -parallel ($name in (Get-AzVmss -ResourceGroupName $AzureResourceGroup).Name)
         {
-            Stop-AzureRmVmss -ResourceGroupName $AzureResourceGroup -VMScaleSetName $name -Force -Verbose
+            Stop-AzVmss -ResourceGroupName $AzureResourceGroup -VMScaleSetName $name -Force -Verbose
         }
     }
     else
     {
 		Write-Output "Starting VMSS in '$($AzureResourceGroup)' resource group";
-        foreach -parallel ($name in (Get-AzureRmVmss -ResourceGroupName $AzureResourceGroup).Name)
+        foreach -parallel ($name in (Get-AzVmss -ResourceGroupName $AzureResourceGroup).Name)
         {
-            Start-AzureRmVmss -ResourceGroupName $AzureResourceGroup -VMScaleSetName $name -Verbose
+            Start-AzVmss -ResourceGroupName $AzureResourceGroup -VMScaleSetName $name -Verbose
         }
     }
 }
